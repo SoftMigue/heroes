f8243ac0ebfaffe2d49cff5ec762c76f336cc957
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-18 20:20:34 +0100

    Class NotFoundExceptionHandler to handle not found items manually

---
+++ src/main/java/cumplido/miguel/heroes/heroes/error/NotFoundExceptionHandler.java
@@ -0,0 +1,10 @@
+package cumplido.miguel.heroes.heroes.error;
+
+
+public class NotFoundExceptionHandler extends Exception {
+
+    public NotFoundExceptionHandler(String message) {
+        super(message);
+    }
+
+}
