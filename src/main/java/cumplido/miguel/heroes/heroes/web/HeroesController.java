120cb13ebb9750ed2542cc9b6ffc3e9b415fcd13
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-18 20:23:07 +0100

    NotFoundExceptionHandler implementation in the controller

--- src/main/java/cumplido/miguel/heroes/heroes/web/HeroesController.java
+++ src/main/java/cumplido/miguel/heroes/heroes/web/HeroesController.java
@@ -1,6 +1,7 @@
 package cumplido.miguel.heroes.heroes.web;
 
 import cumplido.miguel.heroes.heroes.entity.HeroesEntity;
+import cumplido.miguel.heroes.heroes.error.NotFoundExceptionHandler;
 import cumplido.miguel.heroes.heroes.service.DataService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -29,23 +30,23 @@ public class HeroesController {
     }
 
     @GetMapping(GET_HERO_BY_ID_URL)
-    public HeroesEntity getHeroById(@PathVariable int id) {
+    public HeroesEntity getHeroById(@PathVariable int id) throws NotFoundExceptionHandler{
         return dataService.getHeroById(id);
     }
 
     @GetMapping(GET_HERO_BY_TEXT_URL)
-    public List<HeroesEntity> getHeroesByText(@PathVariable String text)  {
+    public List<HeroesEntity> getHeroesByText(@PathVariable String text) throws NotFoundExceptionHandler {
         return dataService.getHeroByText(text);
     }
 
     @PutMapping(UPDATE_HERO_URL)
-    public ResponseEntity<Void> updateHero(@PathVariable int id, @RequestBody String inputHeroName)  {
+    public ResponseEntity<Void> updateHero(@PathVariable int id, @RequestBody String inputHeroName) throws NotFoundExceptionHandler {
         dataService.updateHero(id, inputHeroName);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(DELETE_HERO_URL)
-    public ResponseEntity<Void> deleteHero(@PathVariable int id)  {
+    public ResponseEntity<Void> deleteHero(@PathVariable int id) throws NotFoundExceptionHandler {
         dataService.deleteHero(id);
         return ResponseEntity.noContent().build();
     }
