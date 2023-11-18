1e5e8dbefba38f5f1afe7009c1e28499a9830cb0
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-18 20:10:18 +0100

    data sql file for h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO HEROES (id, name) VALUES (1, 'Batman');
+INSERT INTO HEROES (id, name) VALUES (2, 'MsMarvel');
+INSERT INTO HEROES (id, name) VALUES (3, 'CapitanaMarvel');
+INSERT INTO HEROES (id, name) VALUES (4, 'Thor');
+INSERT INTO HEROES (id, name) VALUES (5, 'Ironman');
