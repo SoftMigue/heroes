23e5b7aeb3bc92c998881328905708d970fd623e
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-18 20:23:19 +0100

    NotFoundExceptionHandler implementation in the data service

--- src/main/java/cumplido/miguel/heroes/heroes/service/DataService.java
+++ src/main/java/cumplido/miguel/heroes/heroes/service/DataService.java
@@ -1,13 +1,14 @@
 package cumplido.miguel.heroes.heroes.service;
 
 import cumplido.miguel.heroes.heroes.entity.HeroesEntity;
+import cumplido.miguel.heroes.heroes.error.NotFoundExceptionHandler;
 
 import java.util.List;
 
 public interface DataService {
     List<HeroesEntity> getAllHeroes();
-    HeroesEntity getHeroById(int id) ;
-    List<HeroesEntity> getHeroByText(String text) ;
-    void updateHero(int id, String inputHeroName) ;
-    void deleteHero(int id) ;
+    HeroesEntity getHeroById(int id) throws NotFoundExceptionHandler;
+    List<HeroesEntity> getHeroByText(String text) throws NotFoundExceptionHandler;
+    void updateHero(int id, String inputHeroName) throws NotFoundExceptionHandler;
+    void deleteHero(int id) throws NotFoundExceptionHandler;
 }
