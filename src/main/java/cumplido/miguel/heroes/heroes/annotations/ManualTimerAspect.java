73ffbd0324ce8e986b3713fca7bcc841a99bec82
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-19 11:29:06 +0100

    creation of ManualTimerAspect

---
+++ src/main/java/cumplido/miguel/heroes/heroes/annotations/ManualTimerAspect.java
@@ -0,0 +1,11 @@
+package cumplido.miguel.heroes.heroes.annotations;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ManualTimerAspect {
+
+
+}
