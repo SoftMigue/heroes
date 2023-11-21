f4d27447e733ef935719fe66cf2dafeb13223665
mmendecu <mmendecu@emeal.nttdata.com>
2023-11-21 18:47:18 +0100

    delete unnecessary javadoc comments

--- src/main/java/cumplido/miguel/heroes/heroes/repository/HeroesRepository.java
+++ src/main/java/cumplido/miguel/heroes/heroes/repository/HeroesRepository.java
@@ -5,19 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-/**
- * Repository interface for accessing and managing hero entities in the database.
- * This interface extends JpaRepository, providing CRUD operations for HeroesEntity.
- */
 @Repository
 public interface HeroesRepository extends JpaRepository<HeroesEntity, Integer> {
 
-    /**
-     * Finds a list of heroes whose names contain the specified text, ignoring case.
-     *
-     * @param text The text to search for in hero names.
-     * @return A list of heroes matching the search criteria.
-     */
     List<HeroesEntity> findByNameContainingIgnoreCase(String text);
 
 }
